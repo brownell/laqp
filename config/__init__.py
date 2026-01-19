201984733dda87cf7e2cf4ee53880165f36cb3c9
Brownell <brownell@chalstrom.com>
2026-01-18 22:13:04 -0600

    deleted ALL the files to start clean with the work from Chromebook last Saturday

--- config/__init__.py
+++
@@ -1,8 +0,0 @@
-# config/__init__.py
-# ============================================================
-"""
-Louisiana QSO Party Configuration Package
-"""
-from .config import *
-
-__version__ = "0.1.0"
